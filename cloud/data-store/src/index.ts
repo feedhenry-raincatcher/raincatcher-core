e19c77abe8a201c23c6b8aa3e094a3d28851998b
Paolo Bueno <paolohaji@gmail.com>
2017-06-06 11:56:51 -0300

    Import code from store spike

---
+++ cloud/data-store/src/index.ts
@@ -0,0 +1,41 @@
+import * as Promise from 'bluebird';
+import * as mongodb from 'mongodb';
+
+export default class MongoStore<T> implements Store<T> {
+  constructor(protected conn: mongodb.Connection) {
+  }
+}
+
+type ID = string | number;
+
+export interface Store<T> {
+  query(): ExecutableChainQuery<T>;
+  or(): ChainQuery<T>;
+  create(data: T): Promise<T>;
+  update(id: ID, data: T): Promise<T>;
+  delete(id: ID): Promise<T>;
+}
+
+interface ChainQuery<T> {
+  // For any supplied key of T,
+  // take a value with the same type as the corresponding property of T
+  where<K extends keyof T>(key: K, value: T[K]): this;
+  gt<K extends keyof T>(key: K, value: T[K]): this;
+  gte<K extends keyof T>(key: K, value: T[K]): this;
+  lt<K extends keyof T>(key: K, value: T[K]): this;
+  lte<K extends keyof T>(key: K, value: T[K]): this;
+  ne<K extends keyof T>(key: K, value: T[K]): this;
+  matches<K extends keyof T>(key: K, value: RegExp): this;
+  or(other: ChainQuery<T>): this;
+
+  limit(max: number): this;
+  skip(amount: number): this;
+
+  sort<K extends keyof T>(key: K, value: 'asc' | 'desc'): this;
+  sortBy<K extends keyof T>(key: K, by: (value: T[K]) => number): this;
+}
+
+interface ExecutableChainQuery<T> extends ChainQuery<T> {
+  get(): Promise<T[]>;
+  getSingle(): Promise<T>;
+}
