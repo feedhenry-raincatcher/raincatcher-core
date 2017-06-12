51aec78bd11328b9075fed75560f7cbeb7ae3036
Jameel Briones <jbriones@localhost.localdomain>
2017-06-12 12:18:26 +0100

    added passport local authentication to demo server routes

--- demo/server/src/routes/index.ts
+++ demo/server/src/routes/index.ts
@@ -1,11 +1,28 @@
 'use strict';
 
 import * as express from 'express';
+import * as passport from 'passport';
+import {loginRequired} from '../auth/local';
 const router = express.Router();
 
-router.get('/', (req: express.Request, res: express.Response) => {
-  const api = { name: 'raincatcher', version: require('../../package.json').version };
+router.get('/', loginRequired, (req: express.Request, res: express.Response) => {
+  const api = { name: 'raincatcher', version: require("../../package.json").version};
   res.json(api);
+});
+
+router.get('/login', (req: express.Request, res: express.Response) => {
+  res.json({name: 'raincatcher-auth', msg: 'You need to login'});
+});
+
+router.post('/login', passport.authenticate('local', {successRedirect: '/', failureRedirect: '/failAuth'}));
+
+router.get('/logout', loginRequired, (req: express.Request, res: express.Response) => {
+  req.logout();
+  res.json({name: 'raincatcher-auth', msg: 'logged out', user: req.user});
+});
+
+router.get('/failAuth', (req: express.Request, res: express.Response) => {
+  res.json({name: 'raincatcher-auth', msg: 'incorrect credentials'});
 });
 
 export default router;
