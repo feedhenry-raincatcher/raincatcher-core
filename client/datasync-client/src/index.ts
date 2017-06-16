259f15007101a280e144648c460f98dee3b4c88c
Wojciech Trocki <wtrocki@redhat.com>
2017-06-16 12:50:39 +0100

    Added support for additional import patterns

--- client/datasync-client/src/index.ts
+++ client/datasync-client/src/index.ts
@@ -1,6 +1,6 @@
 import * as sync from 'fh-sync-js';
 
 // Support named import
-export {sync};
+export { sync };
 // Support named import
 export default sync;
--- cloud/datasync/src/index.ts
+++ cloud/datasync/src/index.ts
@@ -7,5 +7,6 @@ export * from './options/SyncDatasetOptions';
 export * from './SyncApi';
 export * from './web/SyncWebExpress';
 
-export {sync as NativeSync};
+export { sync as NativeSync };
+export { SyncServer };
 export default SyncServer;
