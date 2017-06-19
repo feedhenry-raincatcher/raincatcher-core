250fb0b8662a6b8fca4584076401da0cf0ff04c7
Paolo Bueno <paolohaji@gmail.com>
2017-06-19 10:42:36 -0300

    Add tests and implementation for ProcessInstance#done

--- cloud/wfm/src/process-instance/InstanceImpl.ts
+++ cloud/wfm/src/process-instance/InstanceImpl.ts
@@ -1,8 +1,8 @@
 import * as Promise from 'bluebird';
 import {EventEmitter} from 'eventemitter3';
-import {cloneDeep} from 'lodash';
-import {Task} from '../task';
-import {InstanceTaskEventData, ProcessInstance} from './index';
+import {cloneDeep, every} from 'lodash';
+import {Task, TaskStatus} from '../task';
+import {InstanceEventData, InstanceTaskEventData, ProcessInstance} from './index';
 
 export default class InstanceImpl extends EventEmitter implements ProcessInstance {
   public id: string;
@@ -20,6 +20,10 @@ export default class InstanceImpl extends EventEmitter implements ProcessInstanc
       throw new Error('Task list must have at least one item');
     }
     this.tasks = cloneDeep(initialTasks);
+
+    // subscribe to task events so we can track when they all become 'done'
+    this.tasks.forEach(t => t.on('statusChange', this.checkDone.bind(this)));
+
     this.currentTask = this.tasks[0];
     this.currentTaskIdx = 0;
   }
@@ -34,7 +38,7 @@ export default class InstanceImpl extends EventEmitter implements ProcessInstanc
       instance: this,
       task: this.currentTask
     };
-    this.emit('task:statusChange', e);
+    this.emit('taskChange', e);
     return Promise.resolve(this.currentTask);
   }
 
@@ -44,5 +48,14 @@ export default class InstanceImpl extends EventEmitter implements ProcessInstanc
 
   public getTasks() {
     return Promise.resolve(this.tasks);
+  }
+
+  protected checkDone() {
+    if (every(this.tasks, t => t.getStatus() === TaskStatus.done)) {
+      const e: InstanceEventData<this> = {
+        instance: this
+      };
+      this.emit('done', e);
+    }
   }
 }
