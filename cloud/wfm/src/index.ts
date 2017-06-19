542c90a679025199555eb0b488fafa804f139e38
Paolo Bueno <paolohaji@gmail.com>
2017-06-19 16:22:22 -0300

    Add index.js that just forwards imports

---
+++ cloud/wfm/src/index.ts
@@ -0,0 +1,19 @@
+export * from './executor';
+export * from './process';
+export * from './process-instance';
+export * from './result';
+export * from './task';
+
+import ExecutorImpl from './executor';
+import ProcessImpl from './process';
+import ProcessInstanceImpl from './process-instance';
+import UrlResult from './result';
+import TaskImpl from './task';
+
+export {
+  ExecutorImpl,
+  ProcessImpl,
+  ProcessInstanceImpl,
+  UrlResult,
+  TaskImpl
+};
