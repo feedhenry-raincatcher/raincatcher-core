fca5419bb3a30584c204053788cf931c51c3c712
austincunningham <austincunningham@hotmail.com>
2017-06-21 12:34:58 +0100

    addressing PR comments

--- cloud/logger/test/BunyanLogger.ts
+++ cloud/logger/test/BunyanLogger.ts
@@ -1,12 +1,5 @@
 /// <reference types="mocha" />
 'use strict';
-import * as bunyan from 'bunyan';
-import { expect } from 'chai';
-import * as proxyquire from 'proxyquire';
-
-// const BunyanLogger = proxyquire.noCallThru().load('../src/BunyanLogger', {
-//
-// });
 
 import {BunyanLogger} from '../src/BunyanLogger';
 const loggerObject: object = {test: 'test'};
@@ -16,7 +9,7 @@ const log = new BunyanLogger({name: 'test'});
 
 // call the different end points for logger
 describe('Expected results', () => {
- it('should return the expected responses', () => {
+ it('should log the expected messages', () => {
    log.debug('debug logger message', loggerObject, loggerObject);
    log.error('error logger message', loggerObject);
    log.info('info logger message', loggerObject);
