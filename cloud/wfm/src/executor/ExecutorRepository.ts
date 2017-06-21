3e2262b903a0268d4f3264e97cbab3141e943630
Paolo Bueno <paolohaji@gmail.com>
2017-06-21 17:42:47 -0300

    Add bluebird import

--- cloud/wfm/src/executor/ExecutorRepository.ts
+++ cloud/wfm/src/executor/ExecutorRepository.ts
@@ -1,3 +1,4 @@
+import * as Promise from 'bluebird';
 import ProcessInstance from '../process-instance/ProcessInstance';
 interface ExecutorRepository {
   saveInstance(instance: ProcessInstance): Promise<ProcessInstance>;
