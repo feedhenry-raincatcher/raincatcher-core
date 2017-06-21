fda2ef9efc9feb7e12abf2b1744eeadcef90d28c
austincunningham <austincunningham@hotmail.com>
2017-06-21 12:31:38 +0100

    addressing PR comments

--- cloud/logger/integration/index.ts
+++ cloud/logger/integration/index.ts
@@ -1,14 +1,12 @@
-// integration of logger
-// npm run integration
 
 import {BunyanLogger, Logger} from '../src/index';
 
-// constructor needs at least name
+// constructor accept bunyan options. Please refer to https://www.npmjs.com/package/bunyan
 const log: Logger = new BunyanLogger({name: 'index'});
 // by default debug logging is turned off to enable debug logging change constructor to
 // const log = new BunyanLogger({name: 'index', level: 'debug'});
 
-log.debug('debug logger message\n', {test: 'debug'}, {anything: 'anything'});
-log.error('error logger message\n', {test: 'error'});
-log.info('info logger message\n', {test: 'info'});
-log.warn('warn logger message\n', {test: 'warn'});
+log.debug('debug logger message\n', {testObject: 'debug'}, {anything: 'anything'});
+log.error('error logger message\n', {testObject: 'error'});
+log.info('info logger message\n', {testObject: 'info'});
+log.warn('warn logger message\n', {testObject: 'warn'});
