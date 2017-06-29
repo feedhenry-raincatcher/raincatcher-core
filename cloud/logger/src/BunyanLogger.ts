1a98486c569af1c509d21970081a41a3ed04f978
Austin Cunningham <aucunnin@redhat.com>
2017-06-29 14:25:20 +0100

    issue with BunyanLogger log levels not working , issue with class (#28)

--- cloud/logger/src/BunyanLogger.ts
+++ cloud/logger/src/BunyanLogger.ts
@@ -13,15 +13,15 @@ export class BunyanLogger implements Logger {
   }
 
   public error(format: any, ...params: any[]): void {
-    this.logger.debug(format, params);
+    this.logger.error(format, params);
   }
 
   public info(format: any, ...params: any[]): void {
-    this.logger.debug(format, params);
+    this.logger.info(format, params);
   }
 
   public warn(format: any, ...params: any[]): void {
-    this.logger.debug(format, params);
+    this.logger.warn(format, params);
   }
 }
 
