811c452a50bdcb03e9d84a9eb0376b8a47e28cba
Simon Priadka <spriadka@dhcp130-219.brq.redhat.com>
2017-06-29 14:06:33 +0200

    rm passportauth

--- cloud/passportauth/src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as passport from 'passport';
-
-export * from './user/BaseUser';
-export * from './user/UserApi';
-export * from './user/UserSec';
-export * from './auth/PassportSetup';
-export * from './auth/PassportAuth';
-
-export {passport};
--- cloud/passportauth/src/user/BaseUser.ts
+++
@@ -1,6 +0,0 @@
-export interface BaseUser {
-  id: string;
-  username: string;
-  password: string;
-  roles: string[];
-}
