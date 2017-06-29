426fefe1efa5f6ae39ebde789a6d62a0a0956e42
austincunningham <austincunningham@hotmail.com>
2017-06-29 16:43:34 +0100

    removed js files

--- client/datasync-client/dist/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var sync = require("fh-sync-js");
-exports.sync = sync;
-// Support named import
-exports.default = sync;
-//# sourceMappingURL=index.js.map
