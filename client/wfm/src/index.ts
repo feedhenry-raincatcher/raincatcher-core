854a576d9ebd7bc1bacc82e7c63f39e604cd650e
Paolo Bueno <paolohaji@gmail.com>
2017-07-12 11:21:48 -0300

    Fix spacing

--- client/wfm/src/index.ts
+++ client/wfm/src/index.ts
@@ -1,22 +1,22 @@
 // Interfaces
-export {Executor} from './executor/Executor';
-export {ProcessInstance} from './process-instance/ProcessInstance';
-export {Process} from './process/Process';
-export {Result} from './result/Result';
-export {Task, TaskStatus, TaskEventData} from './task/Task';
-export {TaskHandler} from './task/TaskHandler';
+export { Executor } from './executor/Executor';
+export { ProcessInstance } from './process-instance/ProcessInstance';
+export { Process } from './process/Process';
+export { Result } from './result/Result';
+export { Task , TaskStatus, TaskEventData} from './task/Task';
+export { TaskHandler } from './task/TaskHandler';
 
 // Implementations
-export {ExecutorImpl} from './executor/ExecutorImpl';
-export {ProcessInstanceImpl} from './process-instance/ProcessInstanceImpl';
-export {ProcessImpl} from './process/ProcessImpl';
-export {BooleanResult} from './result/BooleanResult';
-export {UrlResult} from './result/UrlResult';
-export {BaseTask} from './task/BaseTask';
-export {TaskRegistry} from './task/TaskRegistry';
+export { ExecutorImpl } from './executor/ExecutorImpl';
+export { ProcessInstanceImpl } from './process-instance/ProcessInstanceImpl';
+export { ProcessImpl } from './process/ProcessImpl';
+export { BooleanResult } from './result/BooleanResult';
+export { UrlResult } from './result/UrlResult';
+export { BaseTask } from './task/BaseTask';
+export { TaskRegistry } from './task/TaskRegistry';
 
 // Repositories
-export {ExecutorRepository} from './executor/ExecutorRepository';
-export {ProcessRepository} from './process/ProcessRepository';
-export {ProcessInstanceRepository} from './process-instance/ProcessInstanceRepository';
-export {TaskRepository} from './task/TaskRepository';
+export { ExecutorRepository } from './executor/ExecutorRepository';
+export { ProcessRepository } from './process/ProcessRepository';
+export { ProcessInstanceRepository } from './process-instance/ProcessInstanceRepository';
+export { TaskRepository } from './task/TaskRepository';
