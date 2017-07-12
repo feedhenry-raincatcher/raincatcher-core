dc6490e0ab5ee49cf729ddd45d1835a603086327
Paolo Bueno <paolohaji@gmail.com>
2017-07-12 17:42:46 -0300

    Add jsdocs to TaskHandler

--- client/wfm/src/task/TaskHandler.ts
+++ client/wfm/src/task/TaskHandler.ts
@@ -4,7 +4,16 @@ import { Task } from './Task';
 export interface TaskHandler {
   id: string;
   displayName: string;
+  /**
+   * Starts the execution of a Task
+   */
   execute(task: Task, processInstance: ProcessInstance): void;
-  finish(task: Task, processInstance: ProcessInstance): Result;
+  /**
+   * Should be called when a Task is done
+   */
+  next(task: Task, processInstance: ProcessInstance): Result;
+  /**
+   * Should be called upon a user request to move to the previous Task
+   */
   previous(task: Task, processInstance: ProcessInstance): Result;
 }
