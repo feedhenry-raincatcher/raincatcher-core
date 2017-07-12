6d76dc335065a808b556081eef87a2dbc4be2038
Paolo Bueno <paolohaji@gmail.com>
2017-07-12 09:26:42 -0300

    Add dependencies for angular material

--- demo/cloud/src/app/app.module.ts
+++ demo/cloud/src/app/app.module.ts
@@ -1,4 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdSidenavModule, MdButtonModule } from '@angular/material';
+import 'hammerjs';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -8,7 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdSidenavModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
