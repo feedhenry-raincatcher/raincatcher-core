0476e5d1b5962c9e2aaca25b1895c81d4828e039
Paolo Bueno <paolohaji@gmail.com>
2017-07-17 00:38:12 -0300

    Add examples for processinstance and tasks

--- demo/cloud/src/app/app.module.ts
+++ demo/cloud/src/app/app.module.ts
@@ -1,19 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { ButtonsModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { ProcessInstanceComponent } from './process-instance/process-instance.component';
+import { TasksComponent } from './tasks/tasks.component';
+
+// Router and routes setup
+const routes: Routes = [
+  { path: 'process-instances', component: ProcessInstanceComponent },
+  { path: 'tasks', component: TasksComponent },
+  { path: '', redirectTo: '/process-instances', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProcessInstanceComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    ButtonsModule.forRoot()
+    ButtonsModule.forRoot(),
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
