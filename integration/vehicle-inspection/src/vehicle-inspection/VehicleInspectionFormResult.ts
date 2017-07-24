5b11f6a65d1615884d9a7a7489c059f62b76e284
Paolo Bueno <paolohaji@gmail.com>
2017-07-24 14:04:56 -0300

    Remove outdated vehicle inspection example

--- integration/vehicle-inspection/src/vehicle-inspection/VehicleInspectionFormResult.ts
+++
@@ -1,7 +0,0 @@
-import {Result} from '@raincatcher/wfm';
-import {VehicleInspectionFormData} from './VehicleInspectionFormData';
-export class VehicleInspectionFormResult implements Result {
-  public type = 'VehicleInspectionFormResult';
-  constructor(public data: VehicleInspectionFormData) {
-  }
-}
