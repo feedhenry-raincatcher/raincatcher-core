6d7968221f82eb4338d160cbf4d5f5524fcf2d44
Austin Cunningham <aucunnin@redhat.com>
2017-07-25 15:16:57 +0100

    Change Logger example to use both implementations (#59)

--- cloud/logger/example/index.ts
+++ cloud/logger/example/index.ts
@@ -1,3 +1,4 @@
+// npm run example
 import { BunyanLogger, ConsoleLogger, Logger, logger, setLogger } from '../src/index';
 
 // by default logger is turned off
@@ -14,7 +15,18 @@ logger.info('This log will render with BunyanLogger');
 
 // constructor accept bunyan options. Please refer to https://www.npmjs.com/package/bunyan
 
-log.debug('debug logger message\n', {testObject: 'debug'}, {anything: 'anything'});
-log.error('error logger message\n', {testObject: 'error'});
-log.info('info logger message\n', {testObject: 'info'});
-log.warn('warn logger message\n', {testObject: 'warn'});
+logger.debug('debug logger message\n', {testObject: 'debug'}, {anything: 'anything'});
+logger.info('info logger message\n', {testObject: 'info'});
+logger.warn('warn logger message\n', {testObject: 'warn'});
+logger.error('error logger message\n', {testObject: 'error'});
+
+// change the logger to console
+setLogger(new ConsoleLogger());
+logger.warn('=========================================================================================');
+logger.info('info wont render as console default logger level set at warn');
+logger.warn('logger is now set to Console logger and level is warn');
+logger.warn('=========================================================================================');
+// change the logLevel for console log to info
+setLogger(new ConsoleLogger(LogLevel.INFO));
+logger.info('console logger info level will now render');
+logger.info('=========================================================================================');
