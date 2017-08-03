450404f649d0a167535f7db490493203b868449a
Paolo Bueno <paolohaji@gmail.com>
2017-08-03 20:59:37 -0300

    Update jsdocs fro Step

--- client/wfm/src/step/Step.ts
+++ client/wfm/src/step/Step.ts
@@ -1,18 +1,19 @@
 /**
- * Definition for a {@link Task} containing its type identifier and runtime options for customizing behavior.
- * Should be contained in a {@link Process} definition
+ * A single unit of work, containing its identifier and runtime options for customizing behavior.
  */
 export interface Step {
   /**
-   * Code that matches the identifier in {@link TaskHandler}
+   * Code that matches the identifier of the module that should handle the execution of this {@link Step}
    */
   code: string;
   name: string;
   /**
-   * Options object that act as runtime parameters for its equivalent {@link Step}
+   * Options object that act as runtime parameters for the execution of this {@link Step}
    */
   templates?: {
+    /** The template for the {@link Step}'s form, should allow a User to fill the data required for its completion */
     form?: string;
+    /** The template for the {@link Step}'s view, should implement the visualization of its results */
     view?: string;
   };
 }
