d64abc3aa6be6c4c9bee126798a82879685f82d1
Wojciech Trocki <wtrocki@redhat.com>
2017-08-03 11:56:15 +0100

    Remove invalid classes

--- cloud/logger/src/LoggerManager.ts
+++
@@ -1,7 +0,0 @@
-import { Logger } from './Logger';
-
-export let logger: Logger = new Logger();
-// method to set the global logger
-export function setLogger(defaultLogger: Logger) {
-    this.logger = defaultLogger;
-}
