8fcc67b8c6f41d0caeb7a59b3f9457033d2c1fb0
Paolo Bueno <paolohaji@gmail.com>
2017-08-04 11:44:02 -0300

    Port wfmservice from angularjs repo into class

--- client/wfm/src/index.ts
+++ client/wfm/src/index.ts
@@ -3,3 +3,4 @@ export { WorkFlow } from './workflow/WorkFlow';
 export { WorkOrderResult } from './result/WorkOrderResult';
 export { StepResult } from './result/StepResult';
 export { Step } from './step/Step';
+export { STATUS } from './status';
