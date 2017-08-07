b42862adecb15bd2fe001d82676901241ff763ce
Paolo Bueno <paolohaji@gmail.com>
2017-08-07 13:53:02 -0300

    Fix type declaration on DataService interface

--- client/wfm/src/service/DataService.ts
+++ client/wfm/src/service/DataService.ts
@@ -2,7 +2,7 @@ import * as Promise from 'bluebird';
 export interface DataService<T> {
   list(): Promise<T[]>;
   read(id): Promise<T>;
-  create(T): Promise<T>;
-  update(T): Promise<T>;
-  remove(T): Promise<T>;
+  create(item: T): Promise<T>;
+  update(item: T): Promise<T>;
+  remove(item: T): Promise<T>;
 }
