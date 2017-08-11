0186aef835844cb5b8ea8c7c1fbe3efee16786e5
Paolo Bueno <paolohaji@gmail.com>
2017-08-11 20:14:14 -0300

    Delegate to global error handler

--- cloud/wfm-rest-api/src/data-api/ApiError.ts
+++ cloud/wfm-rest-api/src/data-api/ApiError.ts
@@ -3,7 +3,7 @@
  * Interface used to construct standarized response for api error handlers.
  */
 export class ApiError extends Error {
-  constructor(public code: string, public message: string) {
-    super();
+  constructor(public code: string, public message: string, statusCode: number = 500) {
+    super(message);
   }
 }
--- cloud/wfm-rest-api/src/impl/ApiController.ts
+++ cloud/wfm-rest-api/src/impl/ApiController.ts
@@ -1,5 +1,6 @@
 import { getLogger } from '@raincatcher/logger';
 import * as express from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import { ApiError } from '../data-api/ApiError';
 import { defaultPaginationEngine } from '../data-api/MongoPaginationEngine';
 import { PagingDataRepository } from '../data-api/PagingDataRepository';
@@ -10,7 +11,7 @@ import * as errorCodes from './ErrorCodes';
  */
 export class ApiController<T> {
   constructor(
-    readonly router: express.Router,
+    readonly router: Router,
     readonly repository: PagingDataRepository<T>,
     readonly apiPrefix: string) {
   }
@@ -19,113 +20,98 @@ export class ApiController<T> {
    * Handler for list method
    * Can be reused by developers that wish to mount handler directly on router
    */
-  public listHandler(req: express.Request, res: express.Response) {
-    const self = this;
+  public listHandler(req: Request, res: Response, next: NextFunction) {
     getLogger().debug('Api list method called',
-      { object: self.apiPrefix, body: req.query });
+      { object: this.apiPrefix, body: req.query });
     const page = defaultPaginationEngine.buildRequestFromQuery(req.query);
     let filter = {};
-    if (req.query.filter) {
+    if (req.body.filter) {
+      filter = req.body.filter;
+    } else if (req.query.filter) {
       try {
         filter = JSON.parse(req.query.filter);
       } catch (err) {
-        getLogger().debug('Invalid filter passed');
-        const error: ApiError = new ApiError(errorCodes.CLIENT_ERROR, 'Invalid filter query parameter');
-        return res.status(400).json(error);
+        getLogger().error('Invalid filter passed');
+        const error = new ApiError(errorCodes.CLIENT_ERROR, 'Invalid filter query parameter', 400);
+        return next(error);
       }
     }
-    if (req.body.filter) {
-      filter = req.body.filter;
-    }
-    const objectList = self.repository.list(filter, page).then(function(data) {
+    return this.repository.list(filter, page).then(function(data) {
       res.json(data);
-    }).catch(function(err: ApiError) {
-      self.errorHandler(req, res, err);
-    });
+    }).catch(next);
   }
 
   /**
    * Handler for get method
    * Can be reused by developers that wish to mount handler directly on router
    */
-  public getHandler(req: express.Request, res: express.Response) {
-    const self = this;
+  public getHandler(req: Request, res: Response, next: NextFunction) {
     getLogger().debug('Api get method called',
-      { object: self.apiPrefix, params: req.params });
+      { object: this.apiPrefix, params: req.params });
 
     if (!req.params.id) {
-      const error = new ApiError(errorCodes.MISSING_ID, 'Missing id parameter');
-      return res.status(400).json(error);
+      const error = new ApiError(errorCodes.MISSING_ID, 'Missing id parameter', 400);
+      return next(error);
     }
 
-    self.repository.get(req.params.id).then(function(data) {
+    return this.repository.get(req.params.id).then(function(data) {
       res.json(data);
-    }).catch(function(err: ApiError) {
-      self.errorHandler(req, res, err);
-    });
+    }).catch(next);
   }
 
   /**
    * Handler for create method
    * Can be reused by developers that wish to mount handler directly on router
    */
-  public postHandler(req: express.Request, res: express.Response) {
-    const self = this;
+  public postHandler(req: Request, res: Response, next: NextFunction) {
     getLogger().debug('Api create method called',
-      { object: self.apiPrefix, body: req.body });
+      { object: this.apiPrefix, body: req.body });
 
     if (!req.body) {
-      const error = new ApiError(errorCodes.CLIENT_ERROR, 'Missing request body');
-      return res.status(400).json(error);
+      const error = new ApiError(errorCodes.CLIENT_ERROR, 'Missing request body', 400);
+      return next(error);
     }
 
-    self.repository.create(req.body).then(function(data) {
+    return this.repository.create(req.body).then(function(data) {
       res.json(data);
-    }).catch(function(err: ApiError) {
-      self.errorHandler(req, res, err);
-    });
+    }).catch(next);
   }
 
   /**
    * Delete handler
    * Can be reused by developers that wish to mount handler directly on router
    */
-  public deleteHandler(req: express.Request, res: express.Response) {
-    const self = this;
+  public deleteHandler(req: Request, res: Response, next: NextFunction) {
     getLogger().debug('Api delete method called',
-      { object: self.apiPrefix, params: req.params });
+      { object: this.apiPrefix, params: req.params });
 
     if (!req.params.id) {
       const error = new ApiError(errorCodes.MISSING_ID, 'Missing id parameter');
-      return res.status(400).json(error);
+      return next(error);
     }
 
-    self.repository.delete(req.params.id).then(function(data) {
-      res.json();
-    }).catch(function(err) {
-      self.errorHandler(req, res, err);
-    });
+    this.repository.delete(req.params.id).then(function() {
+      // HTTP 204: No Content
+      res.status(204).end();
+    }).catch(next);
   }
 
   /**
    * Update handler
    * Can be reused by developers that wish to mount handler directly on router
    */
-  public putHandler(req: express.Request, res: express.Response) {
-    const self = this;
+  public putHandler(req: Request, res: Response, next: NextFunction) {
     getLogger().debug('Api update method called',
-      { object: self.apiPrefix, body: req.body });
+      { object: this.apiPrefix, body: req.body });
 
     if (!req.body) {
       const error = { code: errorCodes.CLIENT_ERROR, message: 'Missing request body' };
-      return res.status(400).json(error);
+      next(error);
     }
 
-    self.repository.update(req.body).then(function(data) {
+    this.repository.update(req.body).then(function(data) {
       res.json(data);
-    }).catch(function(err: ApiError) {
-      self.errorHandler(req, res, err);
-    });
+    }).catch(next);
   }
 
   /**
@@ -136,7 +122,6 @@ export class ApiController<T> {
    * @param apiPrefix - prefix to mount api in URI path. For example `/prefix/:id`
    */
   public applyAllRoutes() {
-    const self = this;
     const idRoute = this.router.route('/' + this.apiPrefix + '/:id');
     const objectRoute = this.router.route('/' + this.apiPrefix + '/');
     getLogger().info('REST api initialization', this.apiPrefix);
@@ -146,9 +131,5 @@ export class ApiController<T> {
     objectRoute.put(this.putHandler.bind(this));
     idRoute.get(this.getHandler.bind(this));
     idRoute.delete(this.deleteHandler.bind(this));
-  }
-  protected errorHandler(req: express.Request, res: express.Response, error: ApiError) {
-    getLogger().error('Api error', { error, obj: req.body });
-    res.status(500).json(error);
   }
 }
