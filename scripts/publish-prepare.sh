f680e60a3b229e443df0e8c22001b4664a2d3985
Paolo Bueno <paolohaji@gmail.com>
2017-08-25 16:52:20 -0300

    Add comments to bootstrap commands

--- scripts/publish-prepare.sh
+++ scripts/publish-prepare.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -ev
 npm run clean
-lerna bootstrap
+# use devDependencies + hoisting for successful building
+lerna bootstrap -- --no-shrinkwrap
 npm run build
+
 npm run clean:dependencies
+# install only prod dependencies and update lock files
 lerna bootstrap --hoist='' -- --only=production --registry='https://registry.npmjs.org/'
