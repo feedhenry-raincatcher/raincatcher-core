262aa588ec6d7f8b648195b99fcfa1aa131c42ee
Paolo Bueno <paolohaji@gmail.com>
2017-08-29 17:46:39 -0300

    Reinstall root modules too

--- scripts/publish-prepare.sh
+++ scripts/publish-prepare.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -ev
+
+# remove core's node_modules to avoid compilation issues from hoisting
+rm -rf "$(dirname $(readlink -f $0))/../node_modules"
+
+npm install
 npm run clean
 # use devDependencies + hoisting for successful building
 lerna bootstrap -- --no-shrinkwrap
