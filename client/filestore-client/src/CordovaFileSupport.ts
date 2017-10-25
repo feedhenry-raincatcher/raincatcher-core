ea387a1cb24f080d7b55563648c0cf3bf3edb97b
Paolo Bueno <paolohaji@gmail.com>
2017-10-25 11:58:31 -0200

    Use fetch for download

--- client/filestore-client/src/CordovaFileSupport.ts
+++ client/filestore-client/src/CordovaFileSupport.ts
@@ -5,35 +5,13 @@ import * as Bluebird from 'bluebird';
 * @param fileId
 */
 export function downloadFileFromServer(url, fileId) {
-  window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fs) {
-    console.info('file system open: ' + fs.name);
-    fs.root.getFile(fileId, { create: true, exclusive: false }, function(fileEntry) {
-      console.info('fileEntry is file? ' + fileEntry.isFile.toString());
-      const oReq = new XMLHttpRequest();
-      // Make sure you add the domain name to the Content-Security-Policy <meta> element.
-      oReq.open('GET', url + '/' + fileId, true);
-      // Define how you want the XHR data to come back
-      oReq.responseType = 'blob';
-      oReq.onload = function(oEvent) {
-        const blob = oReq.response; // Note: not oReq.responseText
-        if (blob) {
-          // Create a URL based on the blob, and set an <img> tag's src to it.
-          const objUrl = window.URL.createObjectURL(blob);
-          // document.getElementById('bot-img').src = url;
-          // Or read the data with a FileReader
-          // var reader = new FileReader();
-          // reader.addEventListener('loaded', function() {
-          // reader.result contains the contents of blob as text
-          // });
-          // reader.readAsText(blob);
-        } else {
-          console.error('we didnt get an XHR response!');
-        }
-      };
-      oReq.send(null);
-    }, function(err) { console.error('error getting file! ' + err); });
-  }, function(err) { console.error('error getting persistent fs! ' + err); });
-}
+  return new Bluebird<FileSystem>((resolve, reject) => window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, resolve, reject))
+  .then(fs => new Bluebird<FileEntry>((resolve, reject) =>
+    fs.root.getFile(fileId, { create: true, exclusive: false }, resolve, reject)))
+  .then(fileEntry => {
+    return fetch(url).then(response => response.blob()).then(blob => window.URL.createObjectURL(blob));
+  });
+};
 
 /**
 * Upload file using local file URI. Used for uploads on mobile devices (cordova based)
@@ -48,7 +26,6 @@ export function uploadFile(url, fileURI): Promise<Response> {
       reject(new Error('error getting persistent fs! ' + err));
     });
   }).then(function(fs) {
-    // TODO support metadata here
     return new Bluebird<FileEntry>((resolve, reject) => 
     fs.root.getFile(fileURI, { create: false, exclusive: false }, resolve, reject));
   }).then(function(fileEntry) {
