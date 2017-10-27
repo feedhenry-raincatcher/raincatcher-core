7ddfd68ad1f6adffc171b5f15fbd4f03d8c83cdb
Wojciech Trocki <wtrocki@redhat.com>
2017-10-27 14:32:13 +0100

    Http file support

--- client/camera/src/Camera.ts
+++ client/camera/src/Camera.ts
@@ -1,32 +1,47 @@
 'use strict';
 
-import { FileEntry, FileManager } from '@raincatcher/filestore-client';
 import * as Promise from 'bluebird';
 import * as _ from 'lodash';
 
 import { buildCameraOptions } from './buildCameraOptions';
 
+declare var window;
+
+export const TYPE_IMAGE_BASE64 = 'BASE64';
+export const TYPE_IMAGE_URI = 'URI';
+
+/**
+ * Defines response for camera capture
+ */
+export interface CaptureReponse {
+  type: typeof TYPE_IMAGE_BASE64 | typeof TYPE_IMAGE_URI;
+  value: string;
+}
+
 // tslint:disable-next-line:no-var-requires
-const base64ToBlob: (base64: string, contentType: string) => Blob = require('b64-to-blob');
+const base64ToBlob: (base64: string, contentType?: string) => Blob = require('b64-to-blob');
 
 type optionsBuilderFn = (camera: any) => CameraOptions;
 
 export class Camera {
   private initPromise: Promise<CameraOptions>;
-  private fileManager: FileManager;
-  constructor(serverUrl: string, optionsBuilderFunction?: optionsBuilderFn) {
-    this.fileManager = new FileManager(serverUrl, 'camera');
+
+  constructor(optionsBuilderFunction?: optionsBuilderFn) {
     this.init(optionsBuilderFunction);
   }
 
   public init(optionsFn?: optionsBuilderFn): Promise<CameraOptions> {
     return this.initPromise = new Promise((resolve, reject) => {
-      document.addEventListener('deviceready', function() {
-        if (!window.navigator.camera) {
-          return reject(new Error('This module requires the Cordova Camera plugin to be available'));
-        }
-        return resolve();
-      }, false);
+      if (window.cordova) {
+        document.addEventListener('deviceready', function() {
+          if (!window.navigator.camera) {
+            return reject(new Error('This module requires the Cordova Camera plugin to be available'));
+          }
+          return resolve();
+        }, false);
+      } else {
+        resolve();
+      }
     }).then(() => {
       let options = buildCameraOptions(navigator.camera);
       if (_.isFunction(optionsFn)) {
@@ -43,30 +58,20 @@ export class Camera {
       new Promise((resolve, reject) => cleanup(resolve, reject)));
   }
 
-  public capture(): Promise<FileEntry> {
+  public capture(): Promise<CaptureReponse> {
     const getPicture = window.navigator.camera.getPicture;
     const self = this;
     return this.initPromise.then((cameraOptions) =>
       new Promise<string>((resolve, reject) => getPicture(resolve, reject, cameraOptions)))
-      .then(uri => new Promise<string>((resolve, reject) => window.resolveLocalFileSystemURL(uri, function() {
+      .then(uri => new Promise<CaptureReponse>((resolve, reject) => window.resolveLocalFileSystemURL(uri, function() {
         // Cordova's camera plugin only has options to save pictures to the OS' public gallery, shared with other apps
-        return resolve(uri);
+        return resolve({ value: uri, type: TYPE_IMAGE_URI });
       }, function onFileSystemURIError() {
-        // Can be a data-uri when running in a browser,
-        // so resolving will fail
-
+        // Can be a data-uri when running in a browser, resolving will fail
         // in this case, convert the base64 string into an ObjectUrl
-        const blob = base64ToBlob(uri, 'image/jpg');
-        resolve(window.URL.createObjectURL(blob));
+        const blob = base64ToBlob(uri);
+        resolve({ value: window.URL.createObjectURL(blob), type: TYPE_IMAGE_BASE64 });
       }))
-      ).then(function(uri) {
-        const file: FileEntry = {
-          uri
-        };
-        // Fire and forget upload for now
-        // TODO: figure out how to report uploads to UI
-        self.fileManager.scheduleFileToBeUploaded(file);
-        return file;
-      });
+      );
   }
 }
