639b77ed37879325c6e279745720fbd61848dadf
Paolo Bueno <paolohaji@gmail.com>
2017-10-27 10:38:40 -0200

    Receive upload function from client

--- client/filestore-client/src/CordovaFileSupport.ts
+++ client/filestore-client/src/CordovaFileSupport.ts
@@ -1,4 +1,6 @@
 import * as Bluebird from 'bluebird';
+import * as _ from 'lodash';
+import { FileQueueEntry } from './FileQueueEntry';
 
 /**
  * @param url
@@ -18,28 +20,31 @@ export function downloadFileFromServer(url: string, id: string, fileURI: string)
 /**
  * Upload file using local file URI. Used for uploads on mobile devices (cordova based)
  */
-export function uploadFile(url: string, fileURI: string): Promise<Response> {
-  if (arguments.length < 2) {
-    return Bluebird.reject('userId and fileURI parameters are required.');
-  }
-
-  return new Bluebird<FileEntry>((resolve, reject) => window.resolveLocalFileSystemURL(fileURI, function(entry) {
+export function uploadFile(
+  url: string,
+  queueEntry: FileQueueEntry,
+  uploadFn: (url: string, data: FormData) => Promise<Response>
+): Promise<Response> {
+  return new Bluebird<FileEntry>((resolve, reject) => window.resolveLocalFileSystemURL(queueEntry.uri, function(entry) {
     // bug in the file plugin definition?
     resolve(entry as FileEntry);
   }, reject)).then(fileEntry => new Bluebird<Response>(function(resolve, reject) {
-      fileEntry.file(function(file) {
-        const reader = new FileReader();
-        reader.onloadend = function() {
-          // Create a blob based on the FileReader 'result', which we asked to be retrieved as an ArrayBuffer
-          const blob = new Blob([new Uint8Array(this.result)], { type: 'image/jpg' });
-          const data = new FormData();
-          data.append('file', blob);
-          return fetch(url, {
-            method: 'post',
-            body: data
-          }).then(resolve).catch(reject);
-        };
-        reader.readAsArrayBuffer(file);
-      }, reject);
-    }));
+    fileEntry.file(function(file) {
+      const reader = new FileReader();
+      reader.onloadend = function() {
+        _.each(queueEntry, function(value, key) {
+          if (value) {
+            data.append(key, value);
+          }
+        });
+
+        // Create a blob based on the FileReader 'result', which we asked to be retrieved as an ArrayBuffer
+        const blob = new Blob([new Uint8Array(this.result)], { type: 'image/jpg' });
+        const data = new FormData();
+        data.append('file', blob);
+        return uploadFn(queueEntry.uri, data).then(resolve).catch(reject);
+      };
+      reader.readAsArrayBuffer(file);
+    }, reject);
+  }));
 }
