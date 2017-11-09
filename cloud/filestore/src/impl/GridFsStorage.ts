6cbed01c320ccbfb1d03bff242326516f32b96c8
Paolo Bueno <paolobueno@users.noreply.github.com>
2017-11-09 13:39:48 -0200

    [RAINCATCH-1388] Filestore tests (#180)
    * Start refactoring for tests
    
    * Add some tests for filestore
    
    * Add tests for writeStreamToFile
    
    * Finish tests for fileService
    
    * Lower coverage reqs
    
    * Rename storage class to match filename
    
    * Add string type for createTemporaryStorageFolder
    
    * Add first tests for gridfs storage
    
    * Add tests for gridfs
    
    * Update jsdocs
    
    * Update package version to pre-release one
    
    * update dependencies to match hoisted versions
    
    * Fix hanging test

--- cloud/filestore/src/impl/GridFsStorage.ts
+++ cloud/filestore/src/impl/GridFsStorage.ts
@@ -1,5 +1,5 @@
 import { getLogger } from '@raincatcher/logger';
-import * as BlueBird from 'bluebird';
+import * as Promise from 'bluebird';
 import * as fs from 'fs';
 import * as gridfs from 'gridfs-stream';
 import * as mongo from 'mongodb';
@@ -8,38 +8,48 @@ import { Stream } from 'stream';
 import { FileMetadata } from '../file-api/FileMetadata';
 import { FileStorage } from '../file-api/FileStorage';
 
+const connectAsync = Promise.promisify<mongo.Db, string>(MongoClient.connect);
+
 /**
  * Reference implementation for GridFsStorage.
  * See MongoDB documentation for more details.
  */
 export class GridFsStorage implements FileStorage {
 
-  private gridFileSystem: gridfs.Grid;
+  public gridFileSystem: gridfs.Grid;
+  private fileSystemPromise: Promise<gridfs.Grid>;
 
   /**
    * Creates instance of the GridFsStorage that will connect to specified mongo url
-   * @param mongoConnectionURl - MongoDB connection url
+   * @param connection - MongoDB connection url or connection instance
    */
-  constructor(mongoConnectionUrl: string) {
-    const self = this;
-    MongoClient.connect(mongoConnectionUrl, function(err, connection) {
-      if (err) {
-        getLogger().error('Cannot connect to mongodb server. Gridfs storage will be disabled');
-        return;
-      }
-      self.gridFileSystem = gridfs(connection, mongo);
-    });
+  constructor(connection: string | mongo.Db) {
+    if (typeof connection === 'string') {
+      this.fileSystemPromise = connectAsync(connection)
+        .then(conn => this.gridFileSystem = gridfs(conn, mongo));
+    } else {
+      this.gridFileSystem = gridfs(connection, mongo);
+      this.fileSystemPromise = Promise.resolve(this.gridFileSystem);
+    }
+  }
+
+  /**
+   * Gets the underlying gridfs filesystem instance
+   * @return A promise of the underlying fileSystem
+   */
+  public getFileSystem() {
+    return this.fileSystemPromise;
   }
 
   public writeFile(metadata: FileMetadata, fileLocation: string): Promise<string> {
     const self = this;
     if (!self.gridFileSystem) {
-      return BlueBird.reject('Not initialized');
+      return Promise.reject('Not initialized');
     }
     const options = {
       filename: metadata.id
     };
-    return new BlueBird(function(resolve, reject) {
+    return new Promise(function(resolve, reject) {
       const writeStream = self.gridFileSystem.createWriteStream(options);
       writeStream.on('error', function(err) {
         getLogger().error('An error occurred!', err);
@@ -55,12 +65,12 @@ export class GridFsStorage implements FileStorage {
   public readFile(id: string): Promise<Stream> {
     const self = this;
     if (!self.gridFileSystem) {
-      return BlueBird.reject('Not initialized');
+      return Promise.reject('Not initialized');
     }
     const options = {
       filename: id
     };
-    return new BlueBird(function(resolve, reject) {
+    return new Promise(function(resolve, reject) {
       const readstream = self.gridFileSystem.createReadStream(options);
       readstream.on('error', function(err) {
         getLogger().error('An error occurred when reading file from gridfs!', err);
