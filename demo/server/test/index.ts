8ed953fb182bf8b7ea50752e3cf59cd6569501a7
Paolo Bueno <paolobueno@users.noreply.github.com>
2017-11-28 10:27:35 -0200

    WIP [RAINCATCH-1372] Update TSC and other packages (#194)
    * Run package updates
    
    * Update packages on root repo
    
    * Add types mocha to auth module
    
    * Remove faulty typedef
    
    * Fix remainder compilation errors
    
    * Add chai dep to auth
    
    * Make references to base tsconfig.json
    
    * Update update command
    
    * Fix some of the versions via `ncu --removeRange`
    
    * Remove all caret versions

--- demo/server/test/index.ts
+++ demo/server/test/index.ts
@@ -1,18 +1,1 @@
 /// <reference types="mocha" />
-import * as assert from 'assert';
-
-function baseSuite( ) {
-  describe('Base', function() {
-    beforeEach(function() {
-        assert.ok('Test');
-    });
-
-    describe('#foo()', function() {
-      it('foo', function() {
-        assert.ok('Test');
-      });
-    });
-  });
-}
-
-export default baseSuite;
