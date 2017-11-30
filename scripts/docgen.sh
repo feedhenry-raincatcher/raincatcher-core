6b4a4e4aa0777894f1caff2f85e6d18a17fe4606
Paolo Bueno <paolohaji@gmail.com>
2017-11-30 14:19:48 -0200

    Update theme and use typedoc external module plugin

--- scripts/docgen.sh
+++ scripts/docgen.sh
@@ -4,9 +4,12 @@
 VERSION=1.1.0
 DOC_FOLDER=docs
 DOCS_LOCATION=${PWD}/$DOC_FOLDER/api
+TYPEDOC=${PWD}/node_modules/.bin/typedoc # Need to use root typedoc to pick up typedoc plugin
+
+rm -rf $DOCS_LOCATION
 
 echo "Generate documentation for packages"
-lerna exec --ignore @raincatcher/demo-server  -- typedoc --mode modules --out $DOCS_LOCATION/\$LERNA_PACKAGE_NAME --excludePrivate --excludeExternals --theme minimal src/
+lerna exec --ignore @raincatcher/demo-server  -- $TYPEDOC --mode file --out $DOCS_LOCATION/\$LERNA_PACKAGE_NAME --excludePrivate --excludeExternals src/
 
 echo "Rename root folder"
 mv $DOCS_LOCATION/@raincatcher $DOCS_LOCATION/$VERSION
